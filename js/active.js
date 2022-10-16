e67c3774c0892fecd608c25181a11d780b8e4558
Hridoy548 <stuartsunny548@gmail.com>
2022-10-16 16:33:54 +0600

    final add

---
+++ js/active.js
@@ -0,0 +1,10 @@
+//extra_design__mainsec
+
+var extra_design__mainsec = document.querySelector(".extra_design__mainsec");
+var extra_design__mainsechelp = document.querySelector(".extra_design__mainsechelp");
+
+
+extra_design__mainsec.onclick = ()=>{
+    extra_design__mainsechelp.classList.toggle("active");
+} 
+
